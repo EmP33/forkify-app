a96c4d0832dab6fd66fd507fa6ff478a608d1a44
EndiPl2 <purwinmarek16@gmail.com>
2021-12-02 12:45:09 +0100

    Add connection to forkify api

--- dist/index.430fc437.js
+++ dist/index.430fc437.js
@@ -5,7 +5,32 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch(// 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+        'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc958');
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res, data);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        console.dir(err.message);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.430fc437.js.map
